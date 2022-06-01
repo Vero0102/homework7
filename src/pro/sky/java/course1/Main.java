8ac5cb593888b6e14b930c847300fb03ed7c4f51
Vero0102 <104280568+Vero0102@users.noreply.github.com>
2022-06-02 01:44:29 +0300

    Homework 1.7 is ended

--- src/pro/sky/java/course1/Main.java
+++ src/pro/sky/java/course1/Main.java
@@ -3,6 +3,26 @@ package pro.sky.java.course1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //        Домашнее_задание_1.7_Строки
+//        Задание_1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName + ".";
+        System.out.println("ФИО сотрудника — " + fullName);
+
+//        Задание_2
+        String fullNameForAccountReport = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameForAccountReport);
+
+//        Задание_3
+        firstName = "Семён";
+        middleName = "Семёнович";
+        lastName = "Иванов";
+        fullName = lastName.replace('ё', 'е') + " " + firstName.replace('ё', 'е') + " " + middleName.replace('ё', 'е') + ".";
+        fullNameForAccountReport = fullName.toUpperCase();
+        System.out.println("ФИО сотрудника — " + fullName);
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameForAccountReport);
     }
 }
+
